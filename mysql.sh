bf749ca8b1409d03a17e970cd038a8fe528ba5ef
IT1089778 <nvrnagella@gmail.com>
2023-04-05 17:04:14 +0530

    mysql is ready

---
+++ mysql.sh
@@ -0,0 +1,18 @@
+source common.sh
+
+print_head "disable mysql 8 which will come by default with centos 8"
+dnf module disable mysql -y &>> ${LOG
+status_check
+
+print_head "installing mysql"
+yum install mysql-community-server -y &>> ${LOG}
+status_check
+
+print_head "enable and start mysql"
+systemctl enable mysqld &>> ${LOG}
+systemctl restart mysqld
+status_check
+
+print_head "change the default root password in order to start using the database service."
+mysql_secure_installation --set-root-password Roboshop@1
+status_check
