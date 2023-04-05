e7a161d7f0b1f46986a4cdcd992e499bfa91680d
IT1089778 <nvrnagella@gmail.com>
2023-04-05 12:53:12 +0530

    frontend shell script is ready

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,33 @@
 source common.sh
 path_location=$(pwd)
-#print_Head "installing nginx"
-#yum install nginx -y &>> ${LOG}
-#status_check
+print_Head "installing nginx"
+yum install nginx -y &>> ${LOG}
+status_check
 
 print_Head "copying reverse proxy configuration file"
-echo ${path_location}
+cp ${path_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> ${LOG}
 status_check
+
+print_Head "enable and start nginx"
+systemctl enable nginx
+systemctl start nginx
+status_check
+
+print_Head "remove old nginx content"
+rm -rf /usr/share/nginx/html/*
+status_check
+
+print_Head "download nginx content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+status_check
+
+print_Head "navigate to app directory and unzip"
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+status_check
+
+print_Head "restart nginx"
+systemctl restart nginx
+status_check
+
+
