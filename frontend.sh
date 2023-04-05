7d9aa6d1d6c4f83114334d60237ab83c68111aa6
IT1089778 <nvrnagella@gmail.com>
2023-04-05 08:43:57 +0530

    coloring to the text

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,9 @@
 source common.sh
-
-
-print_Head "installing nginx"
+path_location=$(pwd)
+#print_Head "installing nginx"
 #yum install nginx -y &>> ${LOG}
+#status_check
+
+print_Head "copying reverse proxy configuration file"
+echo ${path_location}
 status_check
