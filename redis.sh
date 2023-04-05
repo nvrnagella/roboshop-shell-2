2b8ea46f618f53d1509a9b35041142dd2de9f83d
IT1089778 <nvrnagella@gmail.com>
2023-04-05 15:27:15 +0530

    redis ready

--- redis.sh
+++ redis.sh
@@ -13,7 +13,7 @@ yum install redis -y &>> ${LOG}
 status_check
 
 print_head "updating listed address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf && /etc/redis/redis.conf
 status_check
 
 print_head "enable and start redis"
